2e5604ae72029d828f50b289d5f6126075678986
LuisPatricioGit <luispatricio.info@gmail.com>
2025-06-24 15:27:40 +0100

    Add project files.

---
+++ AppLanches/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AppLanches
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
